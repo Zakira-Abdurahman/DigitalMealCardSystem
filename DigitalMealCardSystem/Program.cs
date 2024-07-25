8d16bdd4eb6159874883900f453f761578d30ebd
Zakira Abdurahman <zakiraabdurahman146@gmail.com>
2024-07-25 12:44:17 +0300

    update program.cs

--- DigitalMealCardSystem/Program.cs
+++ DigitalMealCardSystem/Program.cs
@@ -56,10 +56,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MealCardSystem v1"));
+    app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
